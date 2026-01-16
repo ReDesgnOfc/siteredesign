1ee5ea9d293e3f3e6067831b03c6068b684fc015
renandes15 <redesignerr15@gmail.com>
2026-01-16 17:43:19 -0300

    edição de About Us

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const tabs = document.querySelectorAll('.tab-btn');
-const contents = document.querySelectorAll('.tab-content')
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector("btn2");
+let tabcontent1 = document.querySelector(".tab-content1");
+let tabcontent2 = document.querySelector(".tab-content2");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = tab.getAttribute('data-tab');
-        tabs.forEach(t => t.classList.remove('active'));
-        contents.forEach(c => c.classList.remove('active'));
+btn1.addEventListener("click", function(){
+    btn1.classList.add("active");
+    btn2.classList.remove("active");
+    tabcontent1.style.display = 'block';
+    tabcontent2.style.display = 'none';
+})
 
-        tab.forEach.add('active');
-        document.getElementById(target).classList.add('active')
-    })
+btn2.addEventListener("click", function(){
+    btn1.classList.remove("active");
+    btn2.classList.add("active");
+    tabcontent1.style.display = 'none';
+    tabcontent2.style.display = 'block';
 })
